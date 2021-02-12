c64db64356c8a99c89bcd676a2e57656ac58ff49
MustyMouse <admin@krux.us>
2021-02-11 21:29:56 -0600

    wpe-build update

---
+++ pages/[[...page]].tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  useNextUriInfo,
+  initializeNextServerSideProps,
+} from '@wpengine/headless';
+import { GetServerSidePropsContext } from 'next';
+import Posts from '../lib/components/Posts';
+import Post from '../lib/components/Post';
+
+export default function Page() {
+  const pageInfo = useNextUriInfo();
+
+  if (!pageInfo) {
+    return <></>;
+  }
+
+  if (pageInfo.isPostsPage) {
+    return <Posts />;
+  }
+
+  return <Post />;
+}
+
+export function getServerSideProps(context: GetServerSidePropsContext) {
+  return initializeNextServerSideProps(context);
+}
